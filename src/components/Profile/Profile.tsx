4530cb1375d8770b97adffcebc5e99da4cabb28a
EvgeniBlag <blagodarni@gmail.com>
2022-04-05 16:42:42 +0200

    lastCommit

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import MyPosts from './MyPosts/MyPosts';
+import s from  './Profile.module.css'
+
+
+const Profile = () =>{
+    return <div className={s.content}> 
+    <div>
+       <img src='https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image.jpeg' alt='image'/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <MyPosts/>
+     
+
+        
+      </div>
+    
+}
+
+export default Profile
