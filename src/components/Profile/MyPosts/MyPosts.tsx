cc785107c354576de38eb401cbaba814ca1b5508
EvgeniBlag <blagodarni@gmail.com>
2022-05-23 10:38:37 +0200

    DialogsComponents

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -9,6 +9,9 @@ const MyPosts = () => {
   const posts = [
     { id: 1, message: "Hi its me", likesCount: 12 },
     { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
+    { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
+    { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
+    { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
   ]
 
   let postsElement = posts.map(p => <Post message={p.message} like={p.likesCount} />)
--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,7 +4,9 @@ import s from './ProfileInfo.module.css';
 export const ProfileInfo = () => {
     return (
         <div >
+            
         <img className='profileimg' src='https://3.bp.blogspot.com/-Foj4_IfA_gQ/WPZwxrjmsZI/AAAAAAAACmo/YhFcsEM4jDgLFENHVO30N-wzy3ljuvW_wCLcB/s1600/Gorillaz%2BPandora.jpg' alt='image' />
+      
       </div>
     )
 }
