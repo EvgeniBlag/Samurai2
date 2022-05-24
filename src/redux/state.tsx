702808032696683da005c11641f3dd0d57ad1f7c
EvgeniBlag <blagodarni@gmail.com>
2022-05-24 11:16:35 +0200

    Redux

---
+++ src/redux/state.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+
+export type MyPostsPropsType = {
+    posts: Array<postsType>
+   }
+export type postsType = {
+    id:number,
+    message:string,
+    likesCount:number
+  }
+  
+
+
+  
+  export type DialosPropsType = {
+   dialogs:Array<itemsDialogsType>
+  }
+  export type itemsDialogsType = {
+    id:number,
+    name:string
+  }
+
+
+  
+  export type MessagesPropsType = {
+    messages:Array<itemsMessagesPropsType>
+  }
+  export type itemsMessagesPropsType = {
+    id:number,
+    message:string
+  }
+  
+
+
+const state = {
+    posts: [
+        { id: 1, message: "Hi its me", likesCount: 12 },
+        { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
+        { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
+        { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
+        { id: 2, message: "How is yuor it-kamasutra", likesCount: 15 },
+    ],
+    dialogs : [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "Andrew" },
+        { id: 3, name: "Svetlana" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Victor" },
+        { id: 6, name: "Valera" }
+      ],
+      messages : [
+        { id: 1, message: "Hi its me" },
+        { id: 2, message: "How is yuor it-kamasutra" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "Show me the money" },
+        { id: 5, message: "School of seven bels" },
+        { id: 6, message: "Kiss them for me" }
+      ]
+}
